40f6c21360337ce188c758cb65da0bf46f5b9d52
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-02-26 00:55:53 +0500

    started

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <AppInfo/>
+  </div>
+</template>
+<script>
+import AppInfo from './Components/AppInfo.vue';
+export default {
+  component:{
+    AppInfo
+  }
+}
+</script>
+<style>
+  
+</style>
